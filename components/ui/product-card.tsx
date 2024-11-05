2daeb2cfa2573d120627f3024b325ab3babb58aa
Hitesh <hiteshdahiya81@gmail.com>
2024-11-05 12:05:08 +0530

    Product Page Updated

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -43,10 +43,12 @@ const ProductCard: React.FC<CardProps> = ({ data }) => {
       {/* Images & Icons */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image 
+          priority
           src={data?.images?.[0]?.url}
           alt={data.name}
           fill
           className="aspect-square object-cover rounded-md"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
