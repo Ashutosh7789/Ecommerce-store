b14cf60e8fc2980fcf56795a76c5972b93c5ba0f
Hitesh <hiteshdahiya81@gmail.com>
2024-11-05 12:02:46 +0530

    Cart page added

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -28,6 +28,7 @@ const CartItem: React.FC<CartItemProps> = ({
     <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
         <Image 
+          priority
           fill
           src={data.images[0].url}
           alt={data.name}
